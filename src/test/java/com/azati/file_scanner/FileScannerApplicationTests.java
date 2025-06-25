e6d3b29471827a0793cc52a91b9f8a29cf856f9e
PityulevichArtyom <100440934+PityulevichArtyom@users.noreply.github.com>
2025-06-25 12:26:05 +0300

    Initial commit

---
+++ src/test/java/com/azati/file_scanner/FileScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azati.file_scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
